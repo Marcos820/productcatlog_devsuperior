48db51194adf8c22bad85770bbef46c0f320375b
Marcos Rocha <mvsr.ip@gmail.com>
2021-05-21 21:53:03 -0300

    started the project

---
+++ backend/src/main/java/com/devsuperior/prodcatalog/ProdcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prodcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/prodcatalog/ProdcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.prodcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
